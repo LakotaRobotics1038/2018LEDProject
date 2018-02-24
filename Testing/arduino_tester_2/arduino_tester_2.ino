fd2ab429a08d9818590699bd1c9a2c3ff280fc5e
Marley Pence <marley.pence@gmail.com>
2018-02-24 13:47:18 -0500

    code for the LED testing box

---
+++ Testing/arduino_tester_2/arduino_tester_2.ino
@@ -0,0 +1,49 @@
+#include <Adafruit_NeoPixel.h>
+#include "WS2812_Definitions.h"
+
+#define PIN 4
+#define LED_COUNT 150
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+void setup()
+{
+  // put your setup code here, to run once:
+  leds.begin();
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  cascade (BLUE, TOP_DOWN, 10);
+  cascade (PURPLE, TOP_DOWN, 10);
+}
+
+void cascade(unsigned long color, byte direction, byte wait)
+{
+  if (direction == TOP_DOWN)
+  {
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+      clearLEDs();  // Turn off all LEDs
+      leds.setPixelColor(i, color);  // Set just this one
+      leds.show();
+      delay(wait);
+    }
+  }
+  else
+  {
+    for (int i = LED_COUNT - 1; i >= 0; i--)
+    {
+      clearLEDs();
+      leds.setPixelColor(i, color);
+      leds.show();
+      delay(wait);
+    }
+  }
+}
+void clearLEDs()
+{
+  for (int i = 0; i < LED_COUNT; i++)
+  {
+    leds.setPixelColor(i, 0);
+  }
+}
